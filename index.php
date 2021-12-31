f482bf8e77222e4e397fc60d03c005788bd1299b
Sarus1997 <77056021+Sarus1997@users.noreply.github.com>
2021-12-31 16:55:03 +0700

    Create index.php

---
+++ index.php
@@ -0,0 +1,135 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+
+    <title>Online It Store</title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <script src="https://code.jquery.com/jquery-3.6.0.js"
+        integrity="sha256-H+K7U5CnXl1h5ywQfKtSj8PCmoN9aaq30gDh27Xc0jk=" crossorigin="anonymous"></script>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
+    <link href="https://fonts.googleapis.com/css?family=Lato" rel="stylesheet" type="text/css">
+    <link href="https://fonts.googleapis.com/css?family=Montserrat" rel="stylesheet" type="text/css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
+    <link rel="stylesheet" href="home.css">
+    <link rel="stylesheet" href="login.css">
+
+    <style>
+        .form {
+            display: block;
+            margin-top: 0em;
+        }
+    </style>
+
+</head>
+
+<body id="myPage" data-spy="scroll" data-target=".navbar" data-offset="50">
+
+    <nav class="navbar navbar-default navbar-fixed-top">
+        <div class="container-fluid">
+            <div class="navbar-header">
+                <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#myNavbar">
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                </button>
+                <a class="navbar-brand" href="#myPage">Online It Store</a>
+            </div>
+            <div class="collapse navbar-collapse" id="myNavbar">
+                <ul class="nav navbar-nav navbar-right">
+                    <li><a href="home2.html">HOME</a></li>
+                    <li><a href="about2.html">ABOUT</a></li>
+                    <li><a href="#band">PRODUCT</a></li>
+                    <li><a href="#tour">TROLLEY</a></li>
+                    <li><a href="#contact">CONTACT</a></li>
+                </ul>
+            </div>
+        </div>
+    </nav>
+
+    <div id="myCarousel" class="carousel slide" data-ride="carousel">
+        <ol class="carousel-indicators">
+            <li data-target="#myCarousel" data-slide-to="0" class="active"></li>
+            <li data-target="#myCarousel" data-slide-to="1"></li>
+            <li data-target="#myCarousel" data-slide-to="2"></li>
+        </ol>
+
+        <div class="carousel-inner" role="listbox">
+            <div class="item active">
+                <img src="https://wpadmin.gamefever.co/wp-content/uploads/2020/10/dFQROr7oWzulq5FZUIErQIKM7rH2vOhMMAwOSuvhR6wKJQcSFfQXgcRIh3VzHtN3z94.jpg"
+                    alt="New York" width="1200" height="700">
+                <div class="carousel-caption">
+                    <h2>IPHONE 13 Pro Max</h2>
+                    <p>The Best Smart Phone</p>
+                </div>
+            </div>
+
+            <div class="item">
+                <img src="https://static.thairath.co.th/media/dFQROr7oWzulq5Fa4VljtRL1E0DYcIyxd3Kpbu29goEcnNzuRTTIOor7xDKNkhrQIVX.jpg"
+                    alt="Chicago" width="1200" height="700">
+                <div class="carousel-caption">
+                    <h2>XIAOMI Mi11 ultra</h2>
+                    <p>The Best Smart Phone</p>
+                </div>
+            </div>
+
+            <div class="item">
+                <img src="https://cdn.mos.cms.futurecdn.net/nekFKUzVUDDDgtP2FQGh9Y.jpg" alt="Los Angeles" width="1200"
+                    height="700">
+                <div class="carousel-caption">
+                    <h2>SAMSUNG Galaxy s21 ultra</h2>
+                    <p>The Best Smart Phone</p>
+                </div>
+            </div>
+
+
+        </div>
+
+        <a class="left carousel-control" href="#myCarousel" role="button" data-slide="prev">
+            <span class="glyphicon glyphicon-chevron-left" aria-hidden="true"></span>
+            <span class="sr-only">Previous</span>
+        </a>
+        <a class="right carousel-control" href="#myCarousel" role="button" data-slide="next">
+            <span class="glyphicon glyphicon-chevron-right" aria-hidden="true"></span>
+            <span class="sr-only">Next</span>
+        </a>
+    </div>
+    <br>
+    <br>
+    <br>
+
+
+    <h2 class="text-center">
+        <font size="10">Online It Store</font>
+    </h2>
+    <p class="text-center">
+        <font size="4">เว็บไซต์รวบรวบอุปกรณ์ไอทีมากมาย</font>
+    </p>
+
+    <center>
+        <p>***</p>
+        <button type="submit" class="signupbtn"><a href="login.html">
+                <font size="4" color="green">เข้าสู่ระบบ</font>
+            </a></button>
+        <button type="submit" class="signupbtn"><a href="signin.html">
+                <font size="4" color="red">ลงทะเบียน</font>
+            </a></button>
+    </center>
+
+    </div>
+    <br>
+    <br>
+    <br>
+
+    <footer class="text-center">
+        <a class="up-arrow" href="#myPage" data-toggle="tooltip" title="TO TOP">
+            <span class="glyphicon glyphicon-chevron-up"></span>
+        </a><br><br>
+        <p>ยินดีต้อนรับเข้าสู่ Online It Store</p>
+    </footer>
+
+</body>
+
+</html>
